92bb36f9c54f3d84e4391a9582b83aaf768b55b7
Alan <alan@blurbit.co.uk>
2015-03-21 00:40:54 +0000

    updates to php
    udates

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
-<?php include ("php/form.php");?>
+    <?php
+
+		if($_POST["submit"]){
+						
+			if(!$_POST['name']){
+				$error="<br />Please enter your name";
+			}
+			
+			if(!$_POST['email']){
+				$error.="<br />Please enter your e-mail";
+			}
+			
+			if(!$_POST['comment']){
+				$error.="<br />Please enter a comment";
+			}
+			
+			if($_POST['email']!="" AND !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+				$error.="<br />Please enter a valid e-mail address";
+			}
+			
+			if($error){	
+				$result='<div class="alert alert-danger">There were error(s):'.$error.'</div>';
+			} else{
+				if(mail("alan@blurbit.co.uk", "Comment from Site", "Name: ".$_POST['name'].
+				
+				"Email: ".$_POST['email'].
+				
+				"Comments: ".$_POST['comment']))
+				
+				{
+					$result='<div class="alert alert-success">Thanks for reaching out!</div>';
+				} else{
+					$result='<div class="alert alert-danger">There was an error, please try again</div>';
+				}
+			}
+		}
+
+	?>
 
 <!doctype html>
 <html>
